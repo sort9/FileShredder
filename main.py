4e37790a92c19771e492399586343af8bb119f71
sort9 <143142285+sort9@users.noreply.github.com>
2025-02-11 16:45:00 -0500

    Initial release version

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
 import tkinter as tk
-from tkinter import messagebox, PhotoImage
+from tkinter import messagebox, PhotoImage, filedialog
+import string
+from random import randint
+import os
 
-def startup():
-    messagebox.showinfo("IMPORTANT", "BE CAREFUL AS AFTER A FILE IS SHREDDED, RECOVERING IT WILL BE DIFFICULT")
+# List of alphanumeric values to replace file contents with
+alphanumeric = list(string.ascii_lowercase + string.ascii_uppercase + string.digits)
+random_alphanumeric_index = randint(0, len(alphanumeric) - 1)
 
-def shredComplete():
-    messagebox.showinfo("ALERT", "SHRED COMPLETE")
+def startupMessage():
+    messagebox.showinfo("IMPORTANT", "BE CAREFUL AS AFTER A FILE IS SHREDDED, RECOVERING IT WILL BE DIFFICULT") # Message box for when program start up is complete
+
+def shredCompleteMessage():
+    messagebox.showinfo("ALERT", "SHRED COMPLETE") # Message box for when shred operation is complete
+
+def browsefunc():
+    global filename
+    filename = filedialog.askopenfilename(filetypes=(("txt files", "*.txt"), ("All files", "*.*")))
+    entry_file_input.insert(tk.END, filename)  # Insert the file path into the Entry widget
+
+def shredFile():
+    with open(filename, 'r') as file:
+        global content
+        content = file.read()
+        characterCount = len(content)
+    with open(filename, 'w') as file:
+        for i in range(0, characterCount):
+            content = file.write(alphanumeric[random_alphanumeric_index])
+    os.remove(filename)
+    shredCompleteMessage()
 
 # Main window properties
 root = tk.Tk()
 root.title("Shredder Program")
-root.geometry("400x200") # base window dimensions
-icon = PhotoImage(file='icons/314495_shredder_icon.png') # set program icon set
+root.geometry("450x200") # Base window dimensions
+icon = PhotoImage(file='icons/314495_shredder_icon.png') # Set program icon
 root.iconphoto(True, icon)
+root.config(bg="gray64")
 
 # File Input Entry Area
-label_file_input = tk.Label(root, text="File to shred:")
-label_file_input.pack()
-entry_file_input = tk.Entry(root)
-entry_file_input.pack()
+label_file_input = tk.Label(root, font=("Gill Sans MT", 11, "bold"), text="File to shred:")
+label_file_input.pack(side="left", padx=10, pady=10)
+label_file_input.config(bg="gray64")
+entry_file_input = tk.Entry(root, font=40)
+entry_file_input.pack(side="left")
 
-# Display StartUP
-startup()
+# Browse Button
+browse_button = tk.Button(root, text="Browse", font=40, command=browsefunc)
+browse_button.pack(side="left", padx=5, pady=10)
 
-# run the main loop for then window
+# Shred Button
+shred_button = tk.Button(root, text="Shred", font=40, command=shredFile)
+shred_button.pack(side="left",  padx=5, pady=10)
+
+# Program opens opens in the center of the screen
+root.eval('tk::PlaceWindow . center')
+
+# Display Startup
+startupMessage()
+
+# Run the main loop for then window
 root.mainloop()
